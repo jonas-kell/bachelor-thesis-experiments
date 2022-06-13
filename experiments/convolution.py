cb9ef97872214bbf447ae1542c1b1db012186c12
Jonas Kell <jonas-kell@web.de>
2022-06-14 01:14:02 +0200

    it is (in fact) the same

--- experiments/convolution.py
+++ experiments/convolution.py
@@ -8,21 +8,25 @@ repo: https://github.com/spmallick/learnopencv/tree/master/PyTorch-Fully-Convolu
 import torch
 from torch import nn
 
-conv_1x1 = nn.Conv2d(in_channels=2, out_channels=5, kernel_size=1)
-conv_3x4 = nn.Conv2d(in_channels=2, out_channels=5, kernel_size=(3, 4))
 
 #                         C, H, W
 test_tensor = torch.ones((2, 3, 4))
 print(test_tensor)
 
+# Fully connected layer
 fcl = nn.Linear(in_features=2 * 3 * 4, out_features=5, bias=False)
 print(fcl(test_tensor.reshape((1, 2 * 3 * 4))))  # -> 1, 5
 
+# convolutions
+conv_1x1 = nn.Conv2d(in_channels=2, out_channels=5, kernel_size=1, bias=False)
 print(conv_1x1(test_tensor))  # -> 5, 3, 4
+
+conv_3x4 = nn.Conv2d(in_channels=2, out_channels=5, kernel_size=(3, 4), bias=False)
+conv_3x4.weight.data.copy_((fcl.weight.data.view(5, 2, 3, 4)))
 print(conv_3x4(test_tensor))  # -> 5, 1, 1
 
 conv_1x1_code = nn.Conv2d(
     in_channels=2 * 3 * 4, out_channels=5, kernel_size=1, bias=False
 )
-conv_1x1_code.weight.data.copy_((fcl.weight.data.view(*fcl.weight.data.shape, 1, 1)))
+conv_1x1_code.weight.data.copy_((fcl.weight.data.view(5, 2 * 3 * 4, 1, 1)))
 print(conv_1x1_code(test_tensor.reshape(2 * 3 * 4, 1, 1)))  # -> 5, 1, 1
