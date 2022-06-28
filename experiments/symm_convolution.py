48e4078678ec1533bd7548f83e42391fee18d7e3
Jonas Kell <jonas-kell@web.de>
2022-06-28 09:48:03 +0200

    feat: weigthed backpropagation

--- experiments/symm_convolution.py
+++ experiments/symm_convolution.py
@@ -22,8 +22,8 @@ loss = loss_fn(result, torch.zeros((1, 2, 2)))
 loss.backward()
 optimizer.step()
 
-print(result)
-print(normal_conf.weight.data)
+# print(result)
+# print(normal_conf.weight.data)
 
 # symmetric convolution
 class SymmConf2d(nn.Module):
@@ -31,60 +31,94 @@ class SymmConf2d(nn.Module):
         self,
         in_channels=1,
         out_channels=1,
+        depthwise_seperable_convolution=False,
         has_nn: bool = True,
         has_nnn: bool = True,
     ):
         super().__init__()
 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         self.has_nn = has_nn
         self.has_nnn = has_nnn
 
-        self.center = nn.Conv2d(in_channels, out_channels, 3, bias=False)
+        self.center = nn.Conv2d(
+            self.in_channels,
+            1,
+            3,
+            bias=False,
+        )
         self.center.weight = nn.Parameter(
             torch.tensor(
-                [[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=torch.float32
-            ).reshape(in_channels, out_channels, 3, 3),
+                [[0, 0, 0], [0, 1, 0], [0, 0, 0]] * (self.in_channels),
+                dtype=torch.float32,
+            ).reshape(1, self.in_channels, 3, 3),
             requires_grad=False,
+        )
+        self.center_params = nn.Parameter(
+            Tensor([1] * self.out_channels), requires_grad=True
         )
 
         if has_nn:
-            self.nn = nn.Conv2d(in_channels, out_channels, 3, bias=False)
+            self.nn = nn.Conv2d(self.in_channels, 1, 3, bias=False)
             self.nn.weight = nn.Parameter(
                 torch.tensor(
-                    [[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=torch.float32
-                ).reshape(in_channels, out_channels, 3, 3),
+                    [[0, 1, 0], [1, 0, 1], [0, 1, 0]] * (self.in_channels),
+                    dtype=torch.float32,
+                ).reshape(1, self.in_channels, 3, 3),
                 requires_grad=False,
+            )
+            self.nn_params = nn.Parameter(
+                Tensor([1] * self.out_channels), requires_grad=True
             )
 
         if has_nnn:
-            self.nnn = nn.Conv2d(in_channels, out_channels, 3, bias=False)
+            self.nnn = nn.Conv2d(self.in_channels, 1, 3, bias=False)
             self.nnn.weight = nn.Parameter(
                 torch.tensor(
-                    [[1, 0, 1], [0, 0, 0], [1, 0, 1]], dtype=torch.float32
-                ).reshape(in_channels, out_channels, 3, 3),
+                    [[1, 0, 1], [0, 0, 0], [1, 0, 1]] * (self.in_channels),
+                    dtype=torch.float32,
+                ).reshape(1, self.in_channels, 3, 3),
                 requires_grad=False,
             )
-
-        self.mul_param = nn.Parameter(Tensor([1, 1, 1]), requires_grad=True)
-
-    def forward(self, input: Tensor) -> Tensor:
-        res = self.center(input) * self.mul_param[0]
-
-        if self.has_nn and self.has_nnn:
-            res += (
-                self.nn(input) * self.mul_param[1] + self.nnn(input) * self.mul_param[2]
+            self.nnn_params = nn.Parameter(
+                Tensor([1] * self.out_channels), requires_grad=True
             )
 
-        if self.has_nn and not self.has_nnn:
-            res += self.nn(input) * self.mul_param[1]
+    def forward(self, input: Tensor) -> Tensor:
+        res = torch.einsum(
+            "ijk,i -> ijk",
+            self.center(input).repeat(self.out_channels, 1, 1),
+            self.center_params,
+        )
 
-        if not self.has_nn and self.has_nnn:
-            res += self.nnn(input) * self.mul_param[2]
+        if self.has_nn:
+            res += torch.einsum(
+                "ijk,i -> ijk",
+                self.nn(input).repeat(self.out_channels, 1, 1),
+                self.nn_params,
+            )
+
+        if self.has_nnn:
+            res += torch.einsum(
+                "ijk,i -> ijk",
+                self.nnn(input).repeat(self.out_channels, 1, 1),
+                self.nnn_params,
+            )
 
         return res
 
 
-symm_conf = SymmConf2d(has_nn=True, has_nnn=False)
+channels_in = 2
+channels_out = 3
+x = torch.linspace(
+    1, channels_in * input_size * input_size, channels_in * input_size * input_size
+).reshape(channels_in, input_size, input_size)
+print(x)
+
+symm_conf = SymmConf2d(
+    in_channels=channels_in, out_channels=channels_out, has_nn=True, has_nnn=True
+)
 optimizer = torch.optim.SGD(
     symm_conf.parameters(),
     lr=0.1,
@@ -92,9 +126,15 @@ optimizer = torch.optim.SGD(
 
 optimizer.zero_grad()
 result = symm_conf(x)
-loss = loss_fn(result, torch.zeros((1, 2, 2)))
+loss = loss_fn(result, torch.zeros((channels_out, 2, 2)))
 loss.backward()
 optimizer.step()
 
 print(result)
-print(symm_conf.mul_param.data)
+print(symm_conf.center_params.data)
+print(symm_conf.nn_params.data)
+print(symm_conf.nnn_params.data)
+
+# print(symm_conf.center.weight.data)
+# print(symm_conf.nn.weight.data)
+# print(symm_conf.nnn.weight.data)
