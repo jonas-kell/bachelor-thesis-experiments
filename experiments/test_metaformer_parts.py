7e165896c5649e944b85b329055d4a0531c4a44a
Jonas Kell <jonas-kell@web.de>
2022-07-28 14:47:09 +0200

    test: comparison to jax implementation

--- experiments/test_metaformer_parts.py
+++ experiments/test_metaformer_parts.py
@@ -5,12 +5,13 @@ import sys
 script_dir = os.path.dirname(__file__)
 sys.path.append(os.path.join(script_dir, "../models"))
 
-from metaformer import Attention
+from metaformer import Attention, AveragingConvolutionHead
 
 embed_dim = 8 * 2
 nr_patches = 15
 
-# test attention
+# test Attention
+
 attention = Attention(
     dim=embed_dim,
     num_heads=8,
@@ -21,3 +22,14 @@ attention = Attention(
 x = torch.ones((1, nr_patches, embed_dim))
 
 print(attention(x).shape)
+
+# test Averaging Convolution Head
+
+x = torch.tensor(range(nr_patches * embed_dim)).reshape((1, nr_patches, embed_dim))
+print("x: ", x)
+print("x shape: ", x.shape)
+
+model = AveragingConvolutionHead(in_channels=nr_patches, in_embed_dim=embed_dim)
+
+print(model(x))
+print(model(x).shape)
